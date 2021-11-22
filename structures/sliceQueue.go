98a1192d6f2b963ba3b245d3974f4e2fa13a8c90
krupy <krupy@positron-it.ru>
2021-11-22 22:23:14 +0300

    Added slice queue

---
+++ structures/sliceQueue.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type sliceQueue struct {
+	Queueable
+	items []interface{}
+}
+
+func newSliceQueue(cap int) sliceQueue {
+	return sliceQueue{items: make([]interface{}, 0, cap)}
+}
+
+type Queueable interface {
+	enqueue(value interface{})
+	dequeue()
+	peek() interface{}
+}
+
+func (q *sliceQueue) enqueue(value interface{}) {
+	q.items = append(q.items, value)
+}
+
+func (q *sliceQueue) dequeue() {
+	if len(q.items) == 0 {
+		panic("invalid operation `dequeue` for empty queue")
+	}
+	q.items = q.items[1:len(q.items)]
+}
+
+func (q *sliceQueue) peek() interface{} {
+	if len(q.items) == 0 {
+		panic("invalid operation `peek` for empty queue")
+	}
+	return q.items[0]
+}
+
+func main() {
+	stack := newSliceQueue(8)
+
+	stack.enqueue(6)
+	stack.enqueue(10)
+
+	fmt.Println(stack.peek())
+
+	stack.dequeue()
+
+	fmt.Println(stack.peek())
+}
